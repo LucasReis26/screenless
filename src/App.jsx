3567e27b06e578000424365d410a46c3c823c30a
Lucas Reis <luctreis@gmail.com>
2025-06-09 19:46:11 -0300

    Estruturando App

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import './App.css'
 import Header from './components/Header'
+import Footer from './components/Footer'
+import Container from './components/Container'
 
 function App() {
 
 	return (
 		<>
 			<Header/>
+			<Container/>
+			<Footer/>
 		</>
 	);
 }
