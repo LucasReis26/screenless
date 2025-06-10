b344be12e36f53c35bd7d8c01fb7b81885c4d48f
Lucas Reis <luctreis@gmail.com>
2025-06-09 23:32:30 -0300

    Ultimos ajustes header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,21 +2,19 @@ import styles from './Header.module.css';
 
 function Header(){
 	return(
-		<>
-			<header className={styles.box}>
-				<div>
-					<a href="/">
-						<img src="/ScreenlessLogo.png" alt="Logotipo do Screenless" id="logo"/>
-					</a>
-					<nav className={styles.navItens}>
-						<a href="/sobre-nos">Sobre nós</a>
-						<a href="/servicos">Serviços</a>
-						<a href="/relatos">Relatos</a>
-					</nav>
-				</div>
-				<button className={styles.btn}>Log In</button>
-			</header>
-		</>
+		<header className={styles.box}>
+			<div>
+				<a href="/">
+					<img src="/ScreenlessLogo.png" alt="Logotipo do Screenless" id="logo"/>
+				</a>
+				<nav className={styles.navItens}>
+					<a href="/sobre-nos">Sobre nós</a>
+					<a href="/servicos">Serviços</a>
+					<a href="/relatos">Relatos</a>
+				</nav>
+			</div>
+			<button className={styles.btn}>Log In</button>
+		</header>
 	)
 }
 
