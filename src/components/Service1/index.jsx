b2f4b0c50a43021eeb80a7b5e060e162e367dc1b
Lucas Reis <luctreis@gmail.com>
2025-06-10 01:46:38 -0300

    Imagens adicionadas ao projeto

--- src/components/Service1/index.jsx
+++ src/components/Service1/index.jsx
@@ -5,12 +5,12 @@ function Service1(){
 		<>
 			<div className={styles.service1}>
 				<div className={styles.leftSide}>
-					<h2>Imagine um mundo <br/> com menos telas</h2>
+					<h2>Imagine um mundo <br/> com menos <span>telas!</span></h2>
 					<p>Dê a oportunida à seu filho  de viver <br/> o mundo real e veja ele ser mais feliz.</p>
 				</div>
 				<div className={styles.rightSide}>
-					<img src="" alt="Foto de crianças brincando"/>
-					<img src="" alt="Foto de crianças brincando"/>
+					<img src="/kids-image-1.jpeg" alt="Foto de crianças brincando"/>
+					<img src="/kids-image-2.jpg" alt="Foto de crianças brincando"/>
 					<img src="" alt="Foto de crianças brincando"/>
 					<img src="" alt="Foto de crianças brincando"/>
 					<img src="" alt="Foto de crianças brincando"/>
