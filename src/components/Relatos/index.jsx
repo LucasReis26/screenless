a3a02c55933730390de1186e448ede98c521b1d6
Lucas Reis <luctreis@gmail.com>
2025-06-10 12:04:20 -0300

    Estrutura Relatos concluida

--- src/components/Relatos/index.jsx
+++ src/components/Relatos/index.jsx
@@ -1,9 +1,35 @@
+import CallActionButton from '../CallActionButton';
 import styles from './Relatos.module.css'
 
 function Relatos(){
 	return (
 		<>
-			<h2>Teste</h2>
+			<div className={styles.relatos}>
+				<h2>Nossos <span>Relatos!</span></h2>
+				<div className={styles.relatosContainer}>
+					<div>
+						<img src="" alt="Foto de Perfil de Fulano"/>
+						<h3>Lorem, ipsum dolor.</h3>
+						<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae explicabo perferendis dolores quasi quas cum excepturi velit odio distinctio. Odio quidem deleniti exercitationem delectus magni asperiores, sed quibusdam perspiciatis cum?</p>
+					</div>
+					<div>
+						<img src="" alt="Foto de Perfil de Fulano"/>
+						<h3>Lorem, ipsum dolor.</h3>
+						<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae explicabo perferendis dolores quasi quas cum excepturi velit odio distinctio. Odio quidem deleniti exercitationem delectus magni asperiores, sed quibusdam perspiciatis cum?</p>
+					</div>
+					<div>
+						<img src="" alt="Foto de Perfil de Fulano"/>
+						<h3>Lorem, ipsum dolor.</h3>
+						<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae explicabo perferendis dolores quasi quas cum excepturi velit odio distinctio. Odio quidem deleniti exercitationem delectus magni asperiores, sed quibusdam perspiciatis cum?</p>
+					</div>
+					<div>
+						<img src="" alt="Foto de Perfil de Fulano"/>
+						<h3>Lorem, ipsum dolor.</h3>
+						<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae explicabo perferendis dolores quasi quas cum excepturi velit odio distinctio. Odio quidem deleniti exercitationem delectus magni asperiores, sed quibusdam perspiciatis cum?</p>
+					</div>
+				</div>
+				<CallActionButton/>
+			</div>
 		</>
 	)
 }
