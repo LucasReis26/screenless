df4aa15114e8e447d20b86666dd0bb3e4b042cb9
Lucas Reis <luctreis@gmail.com>
2025-06-10 12:31:57 -0300

    Adding css to relatos

--- src/components/Relatos/index.jsx
+++ src/components/Relatos/index.jsx
@@ -7,25 +7,25 @@ function Relatos(){
 			<div className={styles.relatos}>
 				<h2>Nossos <span>Relatos!</span></h2>
 				<div className={styles.relatosContainer}>
-					<div>
-						<img src="" alt="Foto de Perfil de Fulano"/>
+					<div className={styles.card}>
+						<img src="/profile-pic-fulano.svg" alt="Foto de Perfil de Fulano"/>
 						<h3>Lorem, ipsum dolor.</h3>
-						<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae explicabo perferendis dolores quasi quas cum excepturi velit odio distinctio. Odio quidem deleniti exercitationem delectus magni asperiores, sed quibusdam perspiciatis cum?</p>
+						<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, illum corporis saepe cupiditate molestiae dolores earum? Vel velit debitis labore consequatur neque ratione dolore optio voluptate reiciendis recusandae, officiis quos.</p>
 					</div>
-					<div>
-						<img src="" alt="Foto de Perfil de Fulano"/>
+					<div className={styles.card}>
+						<img src="/profile-pic-fulano.svg" alt="Foto de Perfil de Fulano"/>
 						<h3>Lorem, ipsum dolor.</h3>
-						<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae explicabo perferendis dolores quasi quas cum excepturi velit odio distinctio. Odio quidem deleniti exercitationem delectus magni asperiores, sed quibusdam perspiciatis cum?</p>
+						<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum in soluta officiis sequi, consequuntur hic provident dolor aspernatur atque itaque aperiam commodi minus consectetur, aliquam ducimus molestiae nam accusantium perspiciatis?</p>
 					</div>
-					<div>
-						<img src="" alt="Foto de Perfil de Fulano"/>
+					<div className={styles.card}>
+						<img src="/profile-pic-fulano.svg" alt="Foto de Perfil de Fulano"/>
 						<h3>Lorem, ipsum dolor.</h3>
-						<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae explicabo perferendis dolores quasi quas cum excepturi velit odio distinctio. Odio quidem deleniti exercitationem delectus magni asperiores, sed quibusdam perspiciatis cum?</p>
+						<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam reiciendis libero beatae, corrupti asperiores explicabo cumque repellendus ut eligendi a enim officia quis dolores sequi doloremque numquam animi fuga. Omnis!</p>
 					</div>
-					<div>
-						<img src="" alt="Foto de Perfil de Fulano"/>
+					<div className={styles.card}>
+						<img src="/profile-pic-fulano.svg" alt="Foto de Perfil de Fulano"/>
 						<h3>Lorem, ipsum dolor.</h3>
-						<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae explicabo perferendis dolores quasi quas cum excepturi velit odio distinctio. Odio quidem deleniti exercitationem delectus magni asperiores, sed quibusdam perspiciatis cum?</p>
+						<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus vero sequi itaque illo error velit consequatur recusandae aut cum explicabo, aperiam atque ab. Sequi, expedita? Nemo qui in blanditiis distinctio.</p>
 					</div>
 				</div>
 				<CallActionButton/>
