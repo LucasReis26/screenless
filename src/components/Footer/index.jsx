76dbedb25088b767f7d57387c88b8b6f10708b33
Lucas Reis <luctreis@gmail.com>
2025-06-10 00:14:40 -0300

    Footer finalizado

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -3,17 +3,17 @@ import styles from  './Footer.module.css';
 function Footer(){
 	return (
 		<footer className={styles.footer}>
-			<div>
+			<div className={styles.navegacao}>
 				<h3>Navegação</h3>
 				<nav>
-					<a href="">Sobre nós</a>
-					<a href="">Sobre nós</a>
-					<a href="">Sobre nós</a>
+					<a href="/sobre-nos">Sobre nós</a>
+					<a href="/servicos">Serviços</a>
+					<a href="/relatos">Relatos</a>
 				</nav>
 			</div>
-			<div>
+			<div className={styles.contato}>
 				<h3>Entre em contato</h3>
-				<p>dev@screenless.com</p>
+				<p>contato@screenless.com</p>
 				<p>+55 31 6473-1237</p>
 			</div>
 		</footer>
