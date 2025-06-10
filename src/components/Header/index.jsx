1d9186d193c46cb427d91011ddc88d53182165cd
Lucas Reis <luctreis@gmail.com>
2025-06-09 23:21:04 -0300

    Removendo figure da logo

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -5,9 +5,7 @@ function Header(){
 		<>
 			<header className={styles.box}>
 				<div>
-					<figure>
-						<img src="/ScreenlessLogo.png" alt="Logotipo do Screenless" id="logo"/>
-					</figure>
+					<img src="/ScreenlessLogo.png" alt="Logotipo do Screenless" id="logo"/>
 					<nav className={styles.navItens}>
 						<a href="/sobre-nos">Sobre nós</a>
 						<a href="/servicos">Serviços</a>
