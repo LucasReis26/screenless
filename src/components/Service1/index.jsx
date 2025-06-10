b10d70fb2b213b5785469f2aee22f1753bdd3ddb
Lucas Reis <luctreis@gmail.com>
2025-06-10 02:32:55 -0300

    Css servico um parcialmente concluido

--- src/components/Service1/index.jsx
+++ src/components/Service1/index.jsx
@@ -7,14 +7,19 @@ function Service1(){
 				<div className={styles.leftSide}>
 					<h2>Imagine um mundo <br/> com menos <span>telas!</span></h2>
 					<p>Dê a oportunida à seu filho  de viver <br/> o mundo real e veja ele ser mais feliz.</p>
+					<button>Baixe o App</button>
 				</div>
 				<div className={styles.rightSide}>
-					<img src="/kids-image-1.jpeg" alt="Foto de crianças brincando"/>
-					<img src="/kids-image-2.jpg" alt="Foto de crianças brincando"/>
-					<img src="" alt="Foto de crianças brincando"/>
-					<img src="" alt="Foto de crianças brincando"/>
-					<img src="" alt="Foto de crianças brincando"/>
-					<img src="" alt="Foto de crianças brincando"/>
+					<div className={styles.imgLeft}>
+						<img src="/kids-image-1.jpeg" alt="Foto de crianças brincando"/>
+						<img src="/kids-image-4.jpg" alt="Foto de crianças brincando"/>
+						<img src="/kids-image-6.jpg" alt="Foto de crianças brincando"/>
+					</div>
+					<div className={styles.imgRight}>
+						<img src="/kids-image-2.jpg" alt="Foto de crianças brincando"/>
+						<img src="/kids-image-3.jpg" alt="Foto de crianças brincando"/>
+						<img src="/kids-image-5.jpg" alt="Foto de crianças brincando"/>
+					</div>
 				</div>
 			</div>
 		</>
